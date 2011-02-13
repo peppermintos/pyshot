2cf4709628ee0597c002e4dec8d47af4f3695453
Ikey Doherty <ikey@linuxmint.com>
2011-02-13 17:24:17 +0000

    Modernised UI slightly, added JPEG quality selector (needs testing)

--- usr/lib/pyshot/pyshot.py
+++ usr/lib/pyshot/pyshot.py
@@ -20,250 +20,10 @@ License: GPL
 '''
 class Shotter:
 
-   ui_desc = """<?xml version="1.0"?>
-<interface>
-  <requires lib="gtk+" version="2.16"/>
-  <!-- interface-naming-policy project-wide -->
-  <object class="GtkWindow" id="window">
-    <property name="window_position">center</property>
-    <property name="default_width">400</property>
-    <property name="default_height">230</property>
-    <property name="icon_name">user-desktop</property>
-    <child>
-      <object class="GtkVBox" id="vbox1">
-        <property name="visible">True</property>
-        <property name="orientation">vertical</property>
-        <property name="spacing">3</property>
-        <child>
-          <object class="GtkHBox" id="hbox1">
-            <property name="visible">True</property>
-            <child>
-              <object class="GtkScrolledWindow" id="scrolledwindow1">
-                <property name="visible">True</property>
-                <property name="can_focus">True</property>
-                <property name="hscrollbar_policy">automatic</property>
-                <property name="vscrollbar_policy">automatic</property>
-                <child>
-                  <object class="GtkViewport" id="viewport1">
-                    <property name="visible">True</property>
-                    <property name="resize_mode">queue</property>
-                    <child>
-                      <object class="GtkImage" id="image">
-                        <property name="visible">True</property>
-                        <property name="stock">gtk-missing-image</property>
-                      </object>
-                    </child>
-                  </object>
-                </child>
-              </object>
-              <packing>
-                <property name="position">0</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkVBox" id="vbox2">
-                <property name="visible">True</property>
-                <property name="orientation">vertical</property>
-                <child>
-                  <object class="GtkFrame" id="frame1">
-                    <property name="visible">True</property>
-                    <property name="label_xalign">0</property>
-                    <property name="shadow_type">none</property>
-                    <child>
-                      <object class="GtkAlignment" id="alignment1">
-                        <property name="visible">True</property>
-                        <property name="left_padding">12</property>
-                        <child>
-                          <object class="GtkVBox" id="vbox3">
-                            <property name="visible">True</property>
-                            <property name="orientation">vertical</property>
-                            <child>
-                              <object class="GtkCheckButton" id="checkbutton_hide_win">
-                                <property name="label" translatable="yes">Hide window</property>
-                                <property name="visible">True</property>
-                                <property name="can_focus">True</property>
-                                <property name="receives_default">False</property>
-                                <property name="active">True</property>
-                                <property name="draw_indicator">True</property>
-                              </object>
-                              <packing>
-                                <property name="expand">False</property>
-                                <property name="fill">False</property>
-                                <property name="position">0</property>
-                              </packing>
-                            </child>
-                            <child>
-                              <object class="GtkCheckButton" id="checkbutton_delay">
-                                <property name="label" translatable="yes">Use delay</property>
-                                <property name="visible">True</property>
-                                <property name="can_focus">True</property>
-                                <property name="receives_default">False</property>
-                                <property name="draw_indicator">True</property>
-                              </object>
-                              <packing>
-                                <property name="expand">False</property>
-                                <property name="fill">False</property>
-                                <property name="position">1</property>
-                              </packing>
-                            </child>
-                            <child>
-                              <object class="GtkSpinButton" id="spinbutton">
-                                <property name="visible">True</property>
-                                <property name="sensitive">False</property>
-                                <property name="can_focus">True</property>
-                                <property name="max_length">2</property>
-                                <property name="invisible_char">&#x25CF;</property>
-                                <property name="numeric">True</property>
-                              </object>
-                              <packing>
-                                <property name="expand">False</property>
-                                <property name="fill">False</property>
-                                <property name="position">2</property>
-                              </packing>
-                            </child>
-                          </object>
-                        </child>
-                      </object>
-                    </child>
-                    <child type="label">
-                      <object class="GtkLabel" id="label_options">
-                        <property name="visible">True</property>
-                        <property name="label" translatable="yes">&lt;b&gt;Options&lt;/b&gt;</property>
-                        <property name="use_markup">True</property>
-                      </object>
-                    </child>
-                  </object>
-                  <packing>
-                    <property name="position">0</property>
-                  </packing>
-                </child>
-                <child>
-                  <object class="GtkButton" id="button_shoot">
-                    <property name="visible">True</property>
-                    <property name="can_focus">True</property>
-                    <property name="receives_default">True</property>
-                    <child>
-                      <object class="GtkHBox" id="hbox2">
-                        <property name="visible">True</property>
-                        <child>
-                          <object class="GtkImage" id="image1">
-                            <property name="visible">True</property>
-                            <property name="pixel_size">16</property>
-                            <property name="icon_name">user-desktop</property>
-                          </object>
-                          <packing>
-                            <property name="expand">False</property>
-                            <property name="fill">False</property>
-                            <property name="position">0</property>
-                          </packing>
-                        </child>
-                        <child>
-                          <object class="GtkLabel" id="label_shot">
-                            <property name="visible">True</property>
-                            <property name="label" translatable="yes">Shoot another</property>
-                          </object>
-                          <packing>
-                            <property name="position">1</property>
-                          </packing>
-                        </child>
-                      </object>
-                    </child>
-                  </object>
-                  <packing>
-                    <property name="expand">False</property>
-                    <property name="fill">False</property>
-                    <property name="position">1</property>
-                  </packing>
-                </child>
-                <child>
-                  <object class="GtkButton" id="button_zoom_in">
-                    <property name="label">gtk-zoom-in</property>
-                    <property name="visible">True</property>
-                    <property name="can_focus">True</property>
-                    <property name="receives_default">True</property>
-                    <property name="use_stock">True</property>
-                  </object>
-                  <packing>
-                    <property name="expand">False</property>
-                    <property name="fill">False</property>
-                    <property name="position">2</property>
-                  </packing>
-                </child>
-                <child>
-                  <object class="GtkButton" id="button_zoom_out">
-                    <property name="label">gtk-zoom-out</property>
-                    <property name="visible">True</property>
-                    <property name="can_focus">True</property>
-                    <property name="receives_default">True</property>
-                    <property name="use_stock">True</property>
-                  </object>
-                  <packing>
-                    <property name="expand">False</property>
-                    <property name="fill">False</property>
-                    <property name="position">3</property>
-                  </packing>
-                </child>
-              </object>
-              <packing>
-                <property name="expand">False</property>
-                <property name="fill">False</property>
-                <property name="position">1</property>
-              </packing>
-            </child>
-          </object>
-          <packing>
-            <property name="position">0</property>
-          </packing>
-        </child>
-        <child>
-          <object class="GtkHButtonBox" id="hbuttonbox1">
-            <property name="visible">True</property>
-            <property name="spacing">3</property>
-            <property name="layout_style">end</property>
-            <child>
-              <object class="GtkButton" id="button_close">
-                <property name="label">gtk-close</property>
-                <property name="visible">True</property>
-                <property name="can_focus">True</property>
-                <property name="receives_default">True</property>
-                <property name="use_stock">True</property>
-              </object>
-              <packing>
-                <property name="expand">False</property>
-                <property name="fill">False</property>
-                <property name="position">0</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkButton" id="button_save">
-                <property name="label">gtk-save-as</property>
-                <property name="visible">True</property>
-                <property name="can_focus">True</property>
-                <property name="receives_default">True</property>
-                <property name="use_stock">True</property>
-              </object>
-              <packing>
-                <property name="expand">False</property>
-                <property name="fill">False</property>
-                <property name="position">1</property>
-              </packing>
-            </child>
-          </object>
-          <packing>
-            <property name="expand">False</property>
-            <property name="fill">False</property>
-            <property name="pack_type">end</property>
-            <property name="position">1</property>
-          </packing>
-        </child>
-      </object>
-    </child>
-  </object>
-</interface>"""
-
    def __init__(self):
+      self.ui_file = '/usr/lib/pyshot/interface.ui'
       self.ui = gtk.Builder()
-      self.ui.add_from_string(self.ui_desc)
+      self.ui.add_from_file(self.ui_file)
       self.spinner = self.ui.get_object("spinbutton")
       adj = gtk.Adjustment()
       adj.set_upper(30)
@@ -280,7 +40,7 @@ class Shotter:
       self.ui.get_object("button_save").connect("clicked", self.save_image)
       self.window = self.ui.get_object("window")
       self.window.connect("destroy", gtk.main_quit)
-      self.window.set_title("Screenshot utility")
+      self.window.set_title("Take a screenshot")
       
       self.take_screenie(False, None)
       self.window.show()
@@ -345,14 +105,29 @@ class Shotter:
          self.scale_factor += 0.20
 
    def save_image(self, widget):
+	  # Format options
       format = None
-      frame = gtk.Frame("Image format")
+      frame = gtk.Frame("Image options")
       wid = gtk.VBox()
       jpg = gtk.RadioButton(None, "JPEG format")
       wid.pack_start(jpg, False, False)
       png = gtk.RadioButton(jpg, "PNG format")
       wid.pack_start(png, False, False)
+      
+      # Quality selector
+      adju = gtk.Adjustment(upper=101,lower=1,step_incr=1,page_incr=10,page_size=1,value=100)
+      qual_scale = gtk.HScale(adju)
+      def format_value(widg, value):
+		  return "%d%%" % value
+      qual_scale.connect("format-value", format_value)
+      qual_scale.set_digits(0)
+      qual_box = gtk.HBox()
+      qual_lab = gtk.Label("Quality (JPEG Only)")
+      qual_box.pack_start(qual_lab, False, False, 2)
+      qual_box.pack_start(qual_scale, True, True, 2)
+      wid.pack_start(qual_box, False, False)
       frame.add(wid)
+      
       frame.show_all()
       fc = gtk.FileChooserDialog(title="Save screenshot", parent=self.window, action=gtk.FILE_CHOOSER_ACTION_SAVE,buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_SAVE, gtk.RESPONSE_OK))
       fc.set_extra_widget(frame)
@@ -361,13 +136,14 @@ class Shotter:
          fc.destroy()
       elif(resp == gtk.RESPONSE_OK):
          filetosave = fc.get_filename()
+         quality = qual_scale.get_value()
          fc.destroy()
          if(jpg.get_active()):
             format = "jpeg"
          else:
             format = "png"
             try:
-               self.image.save(filetosave, format)
+               self.image.save(filetosave, format, {"quality" : str(quality) })
             except:
                msg = "Failed to save file: " + filetosave
                md = gtk.MessageDialog(parent=self.window, flags=0, type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK, message_format=msg)
